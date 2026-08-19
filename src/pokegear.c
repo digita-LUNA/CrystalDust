277191f14a7bed761630aa47e0620a1dd4d5f6de
MoriyaFaith <46070717+digita-LUNA@users.noreply.github.com>
2026-08-18 23:42:22 -0400

    UI Updates to Pokegear and Party Menu

--- src/pokegear.c
+++ src/pokegear.c
@@ -881,11 +881,13 @@ static void Task_Pokegear3(u8 taskId)
             PlaySE(SE_POKENAV_OFF);
             return;
         }
-        else if (JOY_NEW(START_BUTTON))
+        else if (JOY_NEW(START_BUTTON)
+            || (JOY_NEW(L_BUTTON) && gSaveBlock2Ptr->optionsButtonMode == OPTIONS_BUTTON_MODE_LR))
         {
             newCard = ChangeCardWithDelta(-1, sPokegearStruct.currentCard);
         }
-        else if (JOY_NEW(SELECT_BUTTON))
+        else if (JOY_NEW(SELECT_BUTTON)
+            || (JOY_NEW(R_BUTTON) && gSaveBlock2Ptr->optionsButtonMode == OPTIONS_BUTTON_MODE_LR))
         {
             newCard = ChangeCardWithDelta(1, sPokegearStruct.currentCard);
         }
